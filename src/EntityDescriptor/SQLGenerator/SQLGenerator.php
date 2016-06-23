65d07d532dd6aab49dc1e4b73c476155283ad9d1
Loenix <florent@sowapps.com>
2016-06-23 18:04:36 +0200

    First commit, imported from Oprheus project with PSR4 fix

---
+++ src/EntityDescriptor/SQLGenerator/SQLGenerator.php
@@ -0,0 +1,30 @@
+<?php
+namespace Oprheus\EntityDescriptor\SQLGenerator;
+
+use Oprheus\EntityDescriptor\EntityDescriptor;
+
+// MySQL Generator
+
+interface SQLGenerator {
+	public function getColumnInfosFromField($field);
+	
+	public function getColumnDefinition($field, $withPK=true);
+	
+	public function getIndexDefinition($index);
+	
+	public function matchEntity(EntityDescriptor $ed);
+	
+	public function getCreate(EntityDescriptor $ed);
+	
+	protected function formatHTML_Command($string);
+	
+	protected function formatHTML_SubCommand($string);
+	
+	protected function formatHTML_ColumnType($string);
+	
+	protected function formatHTML_ReservedWord($string, $class='');
+	
+	protected function formatHTML_Identifier($string);
+	
+	protected function formatHTML_InlineBlock($string, $class);
+}
---
+++ src/EntityDescriptor/User/User.php
@@ -0,0 +1,12 @@
+<?php
+namespace Orpheus\EntityDescriptor\User;
+
+/**
+ * A basic user class
+ * 
+ * The user class represents a basic user, you should never use this class, it was make for documentation.
+ */
+class User extends AbstractUser {
+	
+}
+User::init();
