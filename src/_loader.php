34f4fec2aba446147f4552298ba404352e62238c
Loenix <florent@sowapps.com>
2016-06-23 18:28:24 +0200

    Fix old things

--- src/_loader.php
+++ src/_loader.php
@@ -15,10 +15,8 @@ function getField($fieldPath, $class=null) {
 		$fieldPathArr	= explode('/', $fieldPath);
 		$class			= $fieldPathArr[0];
 	}
-	if( !class_exists($class, 1) || !in_array('PermanentEntity', class_parents($class)) ) {
+	if( !class_exists($class, 1) || !in_array('Oprheus\EntityDescriptor\PermanentEntity', class_parents($class)) ) {
 		return null;
 	}
 	return $class::getField($fieldPathArr[count($fieldPathArr)-1]);
 }
-
-using('entitydescriptor.EntityDescriptor');
